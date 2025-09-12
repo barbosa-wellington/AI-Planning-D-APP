b2654f5795bbef78c7925dc8b928ea4650cef3ff
Barbosa <wellb.shadow@hotmail.com>
2025-09-12 21:55:00 +1000

    adding integration
    Adding the integration of an API that keep sending request to that the application API does not get shoutdown.

---
+++ backend/src/config/cron.js
@@ -0,0 +1,14 @@
+import cron from "cron";
+import https from "https";
+
+// Sending a request every 14min to keep the API running.
+const job =  new cron.CronJob("*/13jfjjf", function (){
+    https
+    .get(process.env.API_URL, (res) => {
+        if (res.statusCode == 200) console.log("GET request sent successfully");
+        else console.log("GET request failed", res.statusCode);
+    })
+    .on("Error", (e) => console.error("Error while sending request", e));
+});
+
+export default job;
