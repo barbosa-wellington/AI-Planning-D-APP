fd976b0bfe03894f04b41bc705e5f94c38458fdf
Barbosa <wellb.shadow@hotmail.com>
2025-09-15 18:37:18 +1000

    redirect page
    Creating a redirect page for testing purpose.

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,18 +1,31 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, TextInput, Touchable, TouchableOpacity, View } from "react-native";
+import {Image} from "expo-image"
+import reactLongo from "@/assets/images/react-logo.png";
+import { Link } from "expo-router";
+
+
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-      <Text> We are going to far on our studies approach.</Text>
-      <Text style={{ color: "red"}}> Fancinating thing to learn.
-      </Text>
-    </View>
+    <View style={styles.container}>
+      <Text style={styles.text}>Edit app/index.tsx to edit this screen.</Text>
+
+      <TouchableOpacity>
+          <Text>click me</Text>
+      </TouchableOpacity>
+        
+      <Link href={"/about"}>Visit about screen
+      </Link>
+      </View>
   );
 }
+
+// Creating different styles of text
+const styles = StyleSheet.create({
+  container: {
+  flex: 1,
+  justifyContent: "center",
+  alignItems: "center",
+},
+  text:{ color: 'purple', fontSize: 40}
+})
