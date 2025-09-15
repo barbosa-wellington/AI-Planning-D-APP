9ad3ef93341bac69990ca9cd22aa734d5e654b6c
Barbosa <wellb.shadow@hotmail.com>
2025-09-15 20:38:40 +1000

    adding style
    Adding style to the about page. SafeAreaView allows the extention of the title and how it should be in size terms.

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,5 +1,14 @@
+import { HeaderShownContext } from "@react-navigation/elements";
 import { Stack } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <Stack screenOptions={{headerShown: false}} />
+    </SafeAreaView>
+
+
+  ) ;
 }
