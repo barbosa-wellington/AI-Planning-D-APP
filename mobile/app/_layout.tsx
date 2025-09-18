b13ec2f2c8a3e6af17da537f82fe8d3a4773a9de
Barbosa <wellb.shadow@hotmail.com>
2025-09-18 21:33:02 +1000

    Adding authentication
    Adding authentication configuration before creating the sign-in/sign-in/log-out access page.

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,13 +1,19 @@
 import { HeaderShownContext } from "@react-navigation/elements";
 import { Stack } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
+// Clerk imports for authentication login
+import { ClerkProvider } from '@clerk/clerk-expo'
+import { Slot } from 'expo-router'
+import { tokenCache } from '@clerk/clerk-expo/token-cache'
+
 
 
 export default function RootLayout() {
   return (
-    <SafeAreaView style={{flex: 1}}>
-      <Stack screenOptions={{headerShown: false}} />
-    </SafeAreaView>
+    // This ClerkProvider function will wrapper-up the application for authentication process.
+    <ClerkProvider tokenCache={tokenCache}>
+    <Slot/>
+    </ClerkProvider>
 
 
   ) ;
