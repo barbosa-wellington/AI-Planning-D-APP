c8f16522835a05775e75c683e93cd1c7cd6b4908
Barbosa <wellb.shadow@hotmail.com>
2025-09-19 21:18:47 +1000

    Adding new file
    Adding new jsx files that will show the screen login,logff, sign-up handle by Clerk authentication.
    
    Bugs were encountered during this process. It was necessary to rebuild old files.

---
+++ mobile/app/(auth)/_layout.tsx
@@ -0,0 +1,13 @@
+// import { Redirect, Stack } from 'expo-router';
+// import { useAuth } from '@clerk/clerk-expo';
+
+// export default function AuthRoutesLayout() {
+//     // This function will validate the user authentication
+//   const { isSignedIn } = useAuth()
+
+//   if (isSignedIn) {
+//     return <Redirect href={'/'} />;
+//   }
+
+//   return <Stack />
+// }
---
+++ mobile/app/(tabs)/index.jsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+const HomeScreen = () => {
+    return (
+        <View>
+            <Text>HomeScreen This is a test page.</Text>
+        </View>
+    );
+};
+export default HomeScreen;
