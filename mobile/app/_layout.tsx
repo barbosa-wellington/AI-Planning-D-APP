c7216ed8c908151a5077a435b471602d10cbe7d5
Barbosa <wellb.shadow@hotmail.com>
2025-09-19 21:51:43 +1000

    styling screen
    Fixing the screenview by defining on the main file _layout.

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,5 +1,3 @@
-import { HeaderShownContext } from "@react-navigation/elements";
-import { Stack } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 // Clerk imports for authentication login
 import { ClerkProvider } from '@clerk/clerk-expo'
@@ -10,9 +8,12 @@ import { tokenCache } from '@clerk/clerk-expo/token-cache'
 
 export default function RootLayout() {
   return (
-    // This ClerkProvider function will wrapper-up the application for authentication process.
+    // This ClerkProvider function will wrapper-up the application for authentication process. 
     <ClerkProvider tokenCache={tokenCache}>
-    <Slot/>
+      {/* Modify the the view so that it will be visible on a screen structure. */}
+    <SafeAreaView style={{ flex: 1}}>
+      <Slot />
+    </SafeAreaView>
     </ClerkProvider>
 
 
