d473bfbe9cd9b5adf34070191cb128e60ff48785
Barbosa <wellb.shadow@hotmail.com>
2025-09-21 17:22:12 +1000

    Sign-in page
    Working on the sign-in page and field authentication.

--- mobile/app/(auth)/sign-in.jsx
+++ mobile/app/(auth)/sign-in.jsx
@@ -1,11 +1,70 @@
-import { View, Text } from 'react-native';
 
+import { View, Text, Alert } from 'react-native';
+import { useRouter } from "expo-router";
+import { useState } from "react";
+import { useSignIn } from "@clerk/clerk-expo";
+
+// import { authStyles } from "../../assets/styles/auth.styles"
 const SignInScreen = () => {
     return (
         <View>
             <Text>This is a test of SignInScreen</Text>
-            {/* <Text>SignInScreen</Text> */}
+            <Text>SignInScreen</Text>
+            <Text>One more line of code</Text>
+            <Text>One more line of code</Text>
         </View>
     );
 };
 export default SignInScreen;
+
+
+
+
+
+// const SignInScreen = () => {
+//     const router = useRouter();
+//     const { signIn, setActive, isLoaded } = userSignIn();
+//     const [email, setEmail] = useState("");
+//     const [password, setPassword] = useState("");
+//     const [showPassword, setShowPassword] = useState(false);
+//     const [loading, setLoading] = useState(False);
+
+//     const handleSignIn = async () => {
+//         if(!email || !password){
+//             Alert.alert("Error", "Please fill in all fields.")
+//             return
+//         }
+
+//         if(isLoaded) return;
+
+//         setLoading(True)
+
+//         try {
+//             const signInAttempt = await signIn.create({
+//                 identifier:email,
+//                 password
+//             })
+
+//             if(signInAttempt.status === "Complete") {
+//                 await setActive({session:signInAttempt.createSessionId})
+//             } else {
+//                 Alert.alert("Error", "Sign in failed. Please try again.");
+//                 console.error(JSON.stringify(signInAttemptm, null, 2));
+//             }
+//         } catch (error) {
+//             Alert.alert("Error", error.errors?.[0]?.message || "Sign in failed");
+//             console.error(JSON.stringify(signInAttemptm, null, 2));
+//         } finally {
+//             setLoading(false)
+
+//         }
+//     }
+//     return (
+//         <View style={authStyles.container}>
+//             <Text>This is a test of SignInScreen</Text>
+//             <Text>SignInScreen</Text>
+//             <Text>SignInScreen</Text>
+//         </View>
+//     );
+// };
+// export default SignInScreen;
