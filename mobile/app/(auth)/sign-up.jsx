bbea21c428cf1123c069b69bdb8324a3049dc421
Barbosa <wellb.shadow@hotmail.com>
2025-09-23 15:33:04 +1000

    Define singUp function
    Create the signUp function and field validation as well as the API of authentication using Clerk.

--- mobile/app/(auth)/sign-up.jsx
+++ mobile/app/(auth)/sign-up.jsx
@@ -1,10 +1,51 @@
-import { View, Text } from 'react-native';
+import { View, Text, Alert } from 'react-native';
+import { useRouter } from 'expo-router';
+import { useSignUp } from "@clerk/clerk-expo";
+import { useState } from 'react';
 
 const SignUpScreen = () => {
+
+    const router = useRouter();
+    const { isLoaded, signUp} = useSignUp;
+    const [ email, setEmail] = useState("");
+    const [ password, setPassword] = useState("");
+    const [ showPassword, setShowPassword] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [pendingVerification, setPendingVerification] = useState(false);
+
+// create a function to handle the signUp bottom action.
+const handleSignUp = async () => {
+    if(!email || ! password) return Alert.alert("Error", "Please fill in all fields");
+    // option check of a password length
+    if (password.length < 6) return Alert.alert("Error", "Password must be at least 6 characters");
+
+    // verify the Clerk authentication 
+    if(!isLoaded) return;
+
+    setLoading(true)
+
+    try {
+        await signUp.create({emailAddress:email, password})
+
+        await signUp.prepareEmailAddressVerification({strategy:"email_code"})
+
+        setPendingVerification(true)
+
+    } catch (error) {
+        Alert.alert("Error", error.error?.[0]?.message || "Failed to create account");
+        console.error(JSON.stringify(error, null, 2));
+        
+    } finally {
+        setLoading(false)
+    }
+
+
+};
+
     return (
         <View>
             <Text>This is a test of SignUpScreen</Text>
-            {/* <Text>SignInScreen</Text> */}
+            <Text>SignInScreen</Text>
         </View>
     );
 };
