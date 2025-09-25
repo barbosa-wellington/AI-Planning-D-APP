c9637a691090cf9ba5983f976b35e711cec143a0
Barbosa <wellb.shadow@hotmail.com>
2025-09-25 17:29:47 +1000

    update package
    Update react-native-reanimated from 4.1.0 to 4.1.1

--- mobile/app/(tabs)/index.jsx
+++ mobile/app/(tabs)/index.jsx
@@ -1,21 +1,13 @@
-import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { View, Text} from "react-native";
 
-import { Ionicons } from '@expo/vector-icons';
-import { router } from 'expo-router';
 
 const HomeScreen = () => {
 
-    // Add this function in your component
-const handleChatPress = () => {
-  // Navigate to chat screen (you'll create this later)
-  router.push('/chat');
-  // Or for now, just log
-  console.log('AI Chat pressed');
-};
+
     return (
         <View>
             <Text>HomeScreen This is a test page.</Text>
-            <Text>HomeScreen This is a test page.</Text>
+            {/* <Text>HomeScreen This is a test page.</Text> */}
 
         </View>
     );
