2e4e4efed4a59040ecc1768cf4d2e25b42ffbf9b
Barbosa <wellb.shadow@hotmail.com>
2025-09-28 13:03:52 +1000

    create feature screen
    Create the file of respective screens.

---
+++ mobile/app/(tabs)/ai_assistant.jsx
@@ -0,0 +1,16 @@
+import { View, Text, TouchableOpacity, StyleSheet, Image } from "react-native";
+import React from "react";
+
+
+const ai_assistant = () => {
+
+
+    return (
+        <View>
+      {/* Page Content */}
+      <Text>AI Assistant This is a test page.</Text>
+
+    </View>
+  );
+};
+export default ai_assistant;
---
+++ mobile/app/(tabs)/food_search.jsx
@@ -0,0 +1,17 @@
+import { View, Text, TouchableOpacity, StyleSheet, Image } from "react-native";
+import React from "react";
+
+
+const food_search = () => {
+
+
+    return (
+        <View>
+      {/* Page Content */}
+      <Text>Food_search This is a test page.</Text>
+      
+
+    </View>
+  );
+};
+export default food_search;
