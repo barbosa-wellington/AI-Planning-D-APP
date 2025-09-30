03f7aac347fcedfed949b3cb5f87d250b09f745b
Barbosa <wellb.shadow@hotmail.com>
2025-09-30 15:01:58 +1000

    API resource
    Collecting API resource for food database

---
+++ mobile/services/mealAPI.js
@@ -0,0 +1,12 @@
+// API-1 = https://fdc.nal.usda.gov/api-guide
+
+
+// API-2 = https://openfoodfacts.github.io/openfoodfacts-server/api/
+
+// API-3 = https://www.foodstandards.gov.au/science-data/food-nutrient-databases/afcd/australian-food-composition-database-download-excel-files
+
+// API-4 = https://www.eurofir.org/our-tools/foodexplorer/
+
+// API-5 = https://fnnbri.quadram.ac.uk/tools/
+
+// API-6 = https://www.tbca.net.br/base-dados-en/search_by_component.php
