de44d0be6a8254f7a6aedefc5ea0eb4d4bb88d26
Barbosa <wellb.shadow@hotmail.com>
2025-10-21 15:01:43 +1100

    fixing model issues
    Ollama cannot troubleshoot, so for some model issue, it is necessary to reinstall the ollama desktop.

--- ai_pipeline/src/models/generate.ts
+++ ai_pipeline/src/models/generate.ts
@@ -2,7 +2,7 @@ import { Ollama} from "ollama";
 
 
 //  Create a function using a local ollama with model Mistral
-// This model requires a machine with minimum of 16GB RAM.
+// This model Gemma3 requires a machine with minimum of 8GB RAM.
 // to run the typescript type npx tsx generate.ts
 
 async function main() {
@@ -10,9 +10,9 @@ async function main() {
 
     //Regular response
     const response = await ollama.generate({
-        model : "Mistral:latest",
+        model : "Gemma3:4b",
         // Prompt determine which response the model will provide.
-        prompt:"Why is the sky blue?"
+        prompt:"Can you say hi to me?"
     });
     console.log(response.response);
 }
--- ai_pipeline/src/models/test-model.py
+++ ai_pipeline/src/models/test-model.py
@@ -7,8 +7,8 @@ import requests
 url  = "http://localhost:11434/api/chat"
 
 payload = {
-    "model": "mistral",
-    "messages":[{"role": "user", "content":"what is Python?"}]
+    "model": "Gemma3:4b",
+    "messages":[{"role": "user", "content":"can you say hi in 5 language? "}]
 }
 
 response = requests.post(url, json=payload, stream=True)
@@ -32,102 +32,3 @@ else:
 
 
 print("testing code using Ollama model")
-
-
-# from ollama import chat
-
-# def get_temperature(city: str) -> str:
-#   """Get the current temperature for a city
-  
-#   Args:
-#     city: The name of the city
-
-#   Returns:
-#     The current temperature for the city
-#   """
-#   temperatures = {
-#     "New York": "22°C",
-#     "London": "15°C",
-#     "Tokyo": "18°C",
-#   }
-#   return temperatures.get(city, "Unknown")
-
-# messages = [{"role": "user", "content": "What's the temperature in New York?"}]
-
-# # pass functions directly as tools in the tools list or as a JSON schema
-# response = chat(model="qwen3", messages=messages, tools=[get_temperature], think=True)
-
-# messages.append(response.message)
-# if response.message.tool_calls:
-#   # only recommended for models which only return a single tool call
-#   call = response.message.tool_calls[0]
-#   result = get_temperature(**call.function.arguments)
-#   # add the tool result to the messages
-#   messages.append({"role": "tool", "tool_name": call.function.name, "content": str(result)})
-
-#   final_response = chat(model="qwen3", messages=messages, tools=[get_temperature], think=True)
-#   print(final_response.message.content)
-
-
-
-# from ollama import chat
-
-# def get_temperature(city: str) -> str:
-#   """Get the current temperature for a city
-  
-#   Args:
-#     city: The name of the city
-
-#   Returns:
-#     The current temperature for the city
-#   """
-#   temperatures = {
-#     "New York": "22°C",
-#     "London": "15°C",
-#     "Tokyo": "18°C",
-#   }
-#   return temperatures.get(city, "Unknown")
-
-# messages = [{"role": "user", "content": "What's the temperature in New York?"}]
-
-# # pass functions directly as tools in the tools list or as a JSON schema
-# response = chat(model="qwen3", messages=messages, tools=[get_temperature], think=True)
-
-# messages.append(response.message)
-# if response.message.tool_calls:
-#   # only recommended for models which only return a single tool call
-#   call = response.message.tool_calls[0]
-#   result = get_temperature(**call.function.arguments)
-#   # add the tool result to the messages
-#   messages.append({"role": "tool", "tool_name": call.function.name, "content": str(result)})
-
-#   final_response = chat(model="qwen3", messages=messages, tools=[get_temperature], think=True)
-#   print(final_response.message.content)
-
-
-
-
-# print("testing Ollama model")
-# url = "https://localhost:11434/generate"
-
-# data = {
-#     "model": "Mistral",
-#     "prompt": "say oi?"}
-
-
-# response = requests.post(url, json=data, stream=True)
-# # check the response status
-
-# if response.status_code == 200:
-#     print("Generated Text: ", end="", flush=True)
-#     # iterate over the streaming response
-#     for l in response.iter_lines():
-#         if l:
-#             #Decode the line and part the json
-#             decoded_line  = l.decode("utf-8")
-#             result = json.loads(decoded_line)
-#             # Get the text from the response
-#             generated_text = result.get("response", "")
-#             print(generated_text, end="", flush=True)
-# else:
-#     print("Error:", response.status_code, response.text)
