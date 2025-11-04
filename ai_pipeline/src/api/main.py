4ca5db9adf344d29b64c836a9a8e62680cffac45
Barbosa <wellb.shadow@hotmail.com>
2025-11-04 20:42:08 +1100

    testing api
    Testing and adding string documentation to the existing code for developers.

--- ai_pipeline/src/api/main.py
+++ ai_pipeline/src/api/main.py
@@ -5,11 +5,16 @@ import ollama
 app = FastAPI()
 
 
-SYSTEM = """ You are an weather specialist who only provides information about wheater. You only provides the current city weahter followed by the temperature in graus celsius and https://www.builder.io/blog/stream-ai-javascript"""
+SYSTEM = """ You are an weather specialist named Dietly. You provide the status of the weather in a given location. 
+You only provides the current city weahter followed by the temperature in graus celsius"""
 
 
 @app.post("/generate")
 def generate(prompt: str):
 
-    response = ollama.chat(model="Gemma3:4b", messages=[{"role":"user", "content": prompt,"system": SYSTEM}])
+    response = ollama.chat(model="Gemma3:4b", messages=[
+        {'role':'system',
+         'content': SYSTEM},
+        {"role":"user", "content": prompt,}
+        ])
     return {"response": response["message"]["content"]}
