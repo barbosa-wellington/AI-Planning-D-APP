354158639240584109cbc3c98940603caa8fe631
Barbosa <wellb.shadow@hotmail.com>
2025-11-13 19:20:23 +1100

    adding screen as project prototype figma

---
+++ mobile/app/user_settings/account.jsx
@@ -0,0 +1,18 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+
+const account = () => {
+
+
+    return (
+        <View>
+      {/* Page Content */}
+      <Text>account This is a test page.</Text>
+      <Text>accout This is a test page.</Text>
+      
+
+    </View>
+  );
+};
+export default account;
